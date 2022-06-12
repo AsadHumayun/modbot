d7118357832d10c43d91e876ab06a3d59ff36ca3
Asad <asad@asadh.dev>
2022-06-12 21:47:25 +0100

    types(Case): add Case type

---
+++ typings/Case.ts
@@ -0,0 +1,16 @@
+import type { Snowflake } from 'discord.js';
+
+type ReasonType = string | "No reason was specified.";
+
+interface Case {
+	id: number;
+	target: string;
+	executor: string;
+	reason: ReasonType;
+	// array of case IDs
+	refers_cases: (number | string)[];
+	guild_id: Snowflake;
+	// Represents an action type.
+	opcode: number;
+	case_log_url: string;
+};
