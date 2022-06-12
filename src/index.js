5df47490dc7382a60ff19191659b3afba5fa0288
Asad <asad@asadh.dev>
2022-06-12 22:08:36 +0100

    feat: add `client.config` and `config.js`

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import './functions/initClient.js';
 import './functions/eventHandler.js';
 import './functions/commandHandler.js';
 
+import config from '../config.js';
 import { client } from './functions/initClient.js';
 import { initDb } from './functions/initDb.js';
 
 client.data = initDb();
+client.config = config;
 
 export {
 	client,
