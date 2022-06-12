9026aa9cda52aa4eacb5ed8d821579e65a6d5a9e
Asad <asad@asadh.dev>
2022-06-12 12:57:39 +0100

    init idk tbh

---
+++ src/functions/initClient.js
@@ -0,0 +1,33 @@
+import { Client, Intents, Options } from "discord.js";
+import { config } from "dotenv";
+
+/**
+ * The currently instantiated Discord Client.
+ * @type {Discord.Client}
+ */
+const client = new Client({
+	// Overriding the cache used in GuildManager, ChannelManager, GuildChannelManager, RoleManager
+	// and PermissionOverwriteManager is unsupported and will break functionality
+	makeCache: Options.cacheWithLimits({
+		MessageManager: 100,
+		GuildMemberManager: 100,
+		PresenceManager: 0,
+		GuildStickerManager: 0,
+		GuildInviteManager: 0,
+		GuildBanManager: 0,
+	}),
+	allowedMentions: { parse: [], repliedUser: false },
+	intents: new Intents([
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MEMBERS,
+		Intents.FLAGS.DIRECT_MESSAGES,
+	]),
+	// enable commands in DMChannels
+	partials: ["CHANNEL"],
+});
+
+config();
+client.login(process.env.token);
+
+export { client };
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * @author asadhumayun
+ */
+
+import "./functions/initClient.js";
+import { client } from "./functions/initClient.js";
+
+console.log(client)
