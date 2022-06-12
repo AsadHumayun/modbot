8c33ce7a2ba70727265fc9b9f6ce122927032c54
Asad <asad@asadh.dev>
2022-06-12 22:10:20 +0100

    chore: indent fix + grammar consistency

--- src/functions/eventHandler.js
+++ src/functions/eventHandler.js
@@ -1,7 +1,7 @@
 /**
  * Iterates through the `events` folder, and binds them to the client.
  * @source {@link https://discordjs.guide/creating-your-bot/event-handling.html}
- * @returns {void}
+* @returns {void}
  */
 
 import { join } from 'node:path';
@@ -10,7 +10,7 @@ import { client } from '../index.js';
 
 const EVENTS_PATH = join(process.cwd(), 'src', 'events');
 const eventFiles = readdirSync(EVENTS_PATH).filter(file => file.endsWith('.js'));
-console.info(`[EventHandler] Detected events directory: ${EVENTS_PATH}`);
+console.info(`[EventHandler] Using events directory: ${EVENTS_PATH}`);
 
 for (const file of eventFiles) {
 	const filePath = join(EVENTS_PATH, file);
