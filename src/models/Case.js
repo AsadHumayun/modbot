8e72b87b17ad23b66b0c756dd6d466ad9824f209
Asad <asad@asadh.dev>
2022-06-12 21:47:43 +0100

    sql(models<Case>): add case model

--- src/models/Case.js
+++ src/models/Case.js
@@ -1,34 +1,38 @@
-import { DataTypes } from "sequelize";
+import { DataTypes } from 'sequelize';
 
 export function Case(sequelize) {
-	return sequelize.define("cases", {
+	return sequelize.define('cases', {
 		id: {
 			unique: true,
 			primaryKey: true,
-			notNull: true,
+			allowNull: false,
 			type: DataTypes.NUMBER,
 		},
 		target: {
 			type: DataTypes.STRING,
-			notNull: true,
+			allowNull: false,
 		},
 		executor: {
 			type: DataTypes.STRING,
-			notNull: true,
+			allowNull: false,
 		},
 		reason: {
 			type: DataTypes.STRING,
-			notNull: true,
+			allowNull: false,
 		},
 		refers_cases: {
 			type: DataTypes.STRING,
 		},
 		guild_id: {
 			type: DataTypes.STRING,
-			notNull: true,
+			allowNull: false,
 		},
 		opcode: {
 			type: DataTypes.NUMBER,
+		},
+		case_log_url: {
+			type: DataTypes.STRING,
+			allowNull: false,
 		},
 	}, {
 		timestamps: true,
