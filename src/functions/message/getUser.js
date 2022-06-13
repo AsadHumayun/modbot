08cac951b228f78d969feb806205b5cceb8eef47
Asad <asad@asadh.dev>
2022-06-13 01:30:13 +0100

    feat: `getUser()` function added!

---
+++ src/functions/message/getUser.js
@@ -0,0 +1,28 @@
+import { client } from '../client/initClient.js';
+
+/**
+ * Extracts the ID of a mentioned user from its raw content
+ * ID is not capitalised in order to keep it in line with Discord.js' naming conventions.
+ * @param {string} mention String to extract mention ID from
+ */
+function _getId(mention) {
+	if (!mention) return;
+	return mention.match(/^<@!?(\d+)>$/)[1];
+}
+
+/**
+ * Fetches a Discord User either by ID or raw mention
+ * @param {string} str The mention - either ID or raw <@(!)id>
+ * @returns {Promise<Discord.User>} Discord user
+ */
+export async function getUser(str) {
+	if (!str) return;
+	let usr;
+	try {
+		usr = await client.users.fetch(_getId(str));
+	}
+	catch (err) {
+		usr = await client.users.fetch(str).catch(() => {return;});
+	}
+	return usr;
+}
