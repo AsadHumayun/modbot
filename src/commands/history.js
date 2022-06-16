1fdbbc074a1ccacb436cea23021c1e58988944c1
Asad <asad@asadh.dev>
2022-06-16 23:33:26 +0100

    backup: before fully completing move to slash cmds

--- src/commands/history.js
+++ src/commands/history.js
@@ -13,7 +13,7 @@ export default {
 			},
 		});
 
-		if (!cases.length) return message.reply(`No cases foud for ${target.tag}`);
+		if (!cases.length) return message.reply(`No cases found for ${target.tag}`);
 		if (!args[1] || args[1] <= 0) args[1] = 25;
 		args[1] = Number(args[1]);
 
@@ -22,17 +22,19 @@ export default {
 			.setTitle(`${target.tag}'s History [${cases.length} cases]`)
 			.setDescription(`To view information on a certain case, use \`${client.config.prefix}case <id>\``);
 
-		const data = await Promise.all(cases.map(async ({ dataValues }) => {
-			const executor = await getUser(dataValues.executor);
-			return {
-				name: `Case #${dataValues.id} | ${executor.tag}`,
-				value: `${dataValues.reason ?? client.config.case.defaultReason}`,
-				inline: true,
-			};
-		}));
-
+		const data = await Promise.all(
+			cases.map(async ({ dataValues }) => {
+				const executor = await getUser(dataValues.executor);
+				return {
+					name: `Case #${dataValues.id} | ${executor.tag}`,
+					value: `${dataValues.reason ?? client.config.case.defaultReason}`,
+					inline: true,
+				};
+			}),
+		);
 		const results = data.reverse().slice(0, args[1]);
-		history.addFields(results);
+		history
+			.addFields(results);
 
 		message.reply({ embeds: [history] });
 	},
