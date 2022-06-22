1510431c8b2d6dfe0ddd580a67e0bc3d9bf697ee
Asad <asad@asadh.dev>
2022-06-23 00:38:00 +0100

    events(debug): fix bug on debug event

--- src/events/debug.js
+++ src/events/debug.js
@@ -1,11 +1,12 @@
 import { WebhookClient } from 'discord.js';
+import { default as config } from '../../config.js';
 
 export default {
 	name: 'debug',
 	once: false,
 	async execute(client, debug) {
-		const webh = new WebhookClient({ url: client.config.debugger['@link'] });
+		const webh = new WebhookClient({ url: config.debugger['@link'] });
 
-		webh.send('`' + debug + '`')
-	}
-}
+		webh.send('`' + debug + '`');
+	},
+};
