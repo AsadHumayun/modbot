4900f2cddf23c9ad03fdfaeddb1b48f7a27546f6
Asad <asad@asadh.dev>
2022-06-27 23:10:16 +0100

    trivial: re-order imports

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import './functions/client/eventHandler.js';
 import './functions/client/commandHandler.js';
 
 import { Collection } from 'discord.js';
-import config from '../config.js';
 import { client } from './functions/client/initClient.js';
 import { initDb } from './functions/client/initDb.js';
+import config from '../config.js';
 
 client.slashCommandData = new Collection();
 client.data = initDb();
