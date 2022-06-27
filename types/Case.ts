7416055df3430a450928c0d138e62913bdf91fec
Asad <asad@asadh.dev>
2022-06-27 23:07:46 +0100

    types(Case): add comments in `Case.ts`

--- types/Case.ts
+++ types/Case.ts
@@ -1,14 +1,41 @@
 import type { Client, Snowflake } from 'discord.js';
 
 interface Case {
+	/**
+	 * Case ID. Unique integer primary key, incremented on each case creation
+	 */
 	id: number;
+	/**
+	 * Discord ID of the user on which this action was executed.
+	 */
 	target: string;
+	/**
+	 * Dicord ID of the user who executed this case.
+	 */
 	executor: string;
+	/**
+	 * The reason for this case.
+	 */
 	reason: string;
+	/**
+	 * References made to other cases.
+	 */
 	refersCases?: string;
+	/**
+	 * The ID of the guild in which this case was carried out in.
+	 */
 	guildId: Snowflake;
+	/**
+	 * The integer opcode value that represents the action carried out.
+	 */
 	opcode: string;
+	/**
+	 * The message URL of the case.
+	 */
 	caseLogURL: string;
+	/**
+	 * Any roles that were either added to or removed from the user in this case.
+	 */
 	roleId?: string;
 };
 
--- types/RawCase.ts
+++ types/RawCase.ts
@@ -5,10 +5,8 @@ interface RawCase {
 	target: string;
 	executor: string;
 	reason: string;
-	// array of case IDs
 	refers_cases?: string;
 	guild_id: Snowflake;
-	// Represents an action type.
 	opcode: string;
 	case_log_url: string;
 	role_id?: string;
