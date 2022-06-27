3e02a053a3a513a217de460c23bffea6a064ea7f
Asad <asad@asadh.dev>
2022-06-27 23:15:37 +0100

    chore: set required as false for options in test command

--- src/SlashCommandData/test.js
+++ src/SlashCommandData/test.js
@@ -2,7 +2,12 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 
 const SlashCommandData = new SlashCommandBuilder()
 	.setName('test')
-	.setDescription('eef43gh4254ed');
+	.setDescription('eef43gh4254ed')
+	.addStringOption(
+		opt => opt
+			.setName('command')
+			.setDescription('tfufytf'),
+	);
 
 export {
 	SlashCommandData as TestSlashCommandData,
