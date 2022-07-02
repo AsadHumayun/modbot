6c6ef8076a91368d027a4a47cd480be607653adf
Asad <asad@asadh.dev>
2022-07-02 02:29:33 +0100

    chore: add event typing to debug event

--- src/events/debug.js
+++ src/events/debug.js
@@ -1,6 +1,9 @@
 import { WebhookClient } from 'discord.js';
 import { default as config } from '../../config.js';
 
+/**
+ * @type {import("../../types/Event").Event}
+ */
 export default {
 	name: 'debug',
 	once: false,
