ddfef9379a303a4ae379f396957f49d96dbc734f
Asad <asad@asadh.dev>
2022-07-02 02:28:42 +0100

    events(guildMemberUpdate): finish, still untested

---
+++ src/events/guildMemberUpdate.js
@@ -0,0 +1,42 @@
+import { getUserData } from '../functions/userData/getUserData.js';
+
+/**
+ * @type {import("../../types/Case").Case}
+ */
+export default {
+	name: 'guildMemberUpdate',
+	once: false,
+	async execute(client, oldMember, newMember) {
+		if (oldMember.guild.id != client.config.guildId) return;
+		// doesn't matter which one we use, oldMember.id and newMember.id will always remain the same.
+		const user = await getUserData(newMember.id);
+		let roles = user.get('roles')?.split(';') || [];
+
+		const oldRoles = [...oldMember.roles.cache.keys()].filter((r) => r != newMember.guild.id);
+		const newRoles = [...newMember.roles.cache.keys()].filter((r) => r != newMember.guild.id);
+
+		for (const f of roles) {
+			if (oldRoles.includes(f) && (!newRoles.includes(f))) {
+				roles = roles.filter((f0) => ![f].includes(f0));
+			}
+		}
+		roles.push(newRoles.filter((f) => !oldRoles.includes(f)).join(';'));
+		newRoles.forEach((f) => {
+			if (!roles.includes(f)) roles.push(f);
+		});
+		for (const f in roles) {
+			if (newMember.guild.roles.cache.get(roles[f]) && (!newRoles.includes(roles[f]))) roles = roles.filter((f0) => ![roles[f]].includes(f0));
+		}
+		roles = roles.filter((f) => !['', client.const.roles.memberRole].includes(f));
+
+		// remove duplicates:
+		roles = [...new Set(roles)];
+		await client.data.Users.update({
+			roles: roles.join(';'),
+		}, {
+			where: {
+				id: oldMember.id,
+			},
+		});
+	},
+};
