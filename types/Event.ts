dd8853f0c8e06a6ced2b7894952284199568ffde
Asad <asad@asadh.dev>
2022-07-04 13:28:28 +0100

    types(Event)

--- types/Event.ts
+++ types/Event.ts
@@ -1,8 +1,19 @@
-import { ClientEvents, type Client } from "discord.js";
+import { ClientEvents, type Client } from 'discord.js';
 
 interface Event {
+	/**
+	 * The name of the event
+	 */
 	name: keyof ClientEvents;
+	/**
+	 * Whether or not this event can be emitted multiple times
+	 */
 	once: boolean;
+	/**
+	 * 
+	 * @param client The currently instantiated Discord client
+	 * @param args The arugments to run this event with (these may vary depending on the event selected)
+	 */
 	execute(client: Client, ...args: unknown[]): Promise<void>;
 }
 
