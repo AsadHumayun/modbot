46797eaa5a0b83ddc1885b73444690a5f6b8737f
Asad <asad@asadh.dev>
2022-07-04 13:32:21 +0100

    events(messageUpdate): add messageUpdate event

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -1,4 +1,5 @@
 import { MessageEmbed } from 'discord.js';
+import { trimStr } from '../utils/string/trimStr.js';
 
 /**
  * @type {import('../../types/Event').Event}
@@ -10,15 +11,17 @@ export default {
 		if (newMessage.partial) newMessage = await newMessage.fetch();
 		if (oldMessage.channel.type == 'DM') return;
 		if ((oldMessage.guild.id != client.config.guildId || (oldMessage.author?.bot) || (oldMessage.content === newMessage.content))) return;
-		client.channels.cache.get(client.const.channels.msgLogs).send({
+		client.channels.cache.get(client.config.channels.msglog).send({
 			embeds: [
 				new MessageEmbed()
 					.setAuthor({ name: `${newMessage.author.tag} (${newMessage.author.id})`, iconURL: newMessage.author.displayAvatarURL({ dynamic: true }) })
 					.setTitle(`Message Edited in #${oldMessage.channel.name}`)
 					.setThumbnail(oldMessage.author.displayAvatarURL())
 					.setColor('RANDOM')
-					.addField('Old Message', client.utils.trim(oldMessage.content, 1024), true)
-					.addField('New Message', client.utils.trim(newMessage.content, 1024), true)
+					.addFields(
+						{ name: 'Old Message', value: trimStr(oldMessage.content, 1024), inline: true },
+						{ name: 'New Message', value: trimStr(newMessage.content, 1024), inline: true },
+					)
 					.setFooter({ text: `Message ID: ${newMessage.id}`, iconURL: newMessage.guild.iconURL({ dynamic: true }) })
 					.setTimestamp(),
 			],
