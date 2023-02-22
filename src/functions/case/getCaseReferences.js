aaaf479ff511466feb53c07c4f352401b88d0981
Asad <asad@asadh.dev>
2023-02-22 10:21:32 +0000

    getCaseReferences: fix typo on JSDoc comment

--- src/functions/case/getCaseReferences.js
+++ src/functions/case/getCaseReferences.js
@@ -3,7 +3,7 @@ import { client } from '../client/initClient.js';
 
 /**
  * Returns an array of hyperlinked case references. Checks are carried out here to ensure that the length of the returned string
- * does not exist that of further constraints. (e.g. Discord imposes a 6)
+ * does not exist that of further constraints. (e.g. Discord imposes a 6000 character limit on embeds)
  * @param {number[]} cases array of case IDs to compile references for
  * @param {?number} prevChars amount of chars that will have preceded this and should be included in the char count
  * @param {number} charLimit char limit - once reached, appends "..." and leaves it at that.
@@ -23,9 +23,8 @@ export async function getCaseReferences(cases, prevChars = 0, charLimit) {
 
 	for (const { dataValues } of data) {
 		const ref = `[#${dataValues.id}](${dataValues.case_log_url})`;
-		if (chars >= charLimit) break;
-		if (chars + ref.length >= charLimit) {
-			if (chars + 3 >= charLimit) break;
+		if (chars + ref.length > charLimit) {
+			if (chars + 3 > charLimit) break;
 			final.push('...');
 			break;
 		}
