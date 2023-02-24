f0f5b56d8398a7e2ff03fc1a06db472159a57122
Asad <asad@asadh.dev>
2023-02-24 21:43:54 +0000

    events(ready): Add Event type to ready event

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,9 @@
 import { reloadApplicationCommands } from '../functions/client/reloadApplicationCommands.js';
 import { setPresence } from '../functions/client/setPresence.js';
 
+/**
+ * @type {import('../../types/Event').Event}
+ */
 export default {
 	name: 'ready',
 	once: false,
