cf7fc8eb5623a358c9792e9e8e61861bfc318967
Asad <asad@asadh.dev>
2023-03-05 02:16:32 +0000

    types(RawCase): Make some properties use type `Snowflake`

--- types/RawCase.ts
+++ types/RawCase.ts
@@ -2,15 +2,15 @@ import type { Snowflake } from 'discord.js';
 
 interface RawCase {
 	id: number;
-	target: string;
-	executor: string;
+	target: Snowflake;
+	executor: Snowflake;
 	reason: string;
 	refers_cases?: string;
 	guild_id: Snowflake;
 	opcode: string;
 	case_log_url: string;
-	role_id?: string;
-	prune_members_kicked: number;
+	role_id?: Snowflake;
+	prune_members_kicked?: number;
 	prune_included_roles?: string;
 	prune_days?: number;
 	channel_id?: Snowflake;
